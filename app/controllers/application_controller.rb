88e8068284c6a15826077d9b03124bcd5d6c526c
ayano-w <ayasaku.t5@gmail.com>
2021-10-17 23:25:56 +0000

    [Start]4-1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameterts, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parameterts
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
