2b3d651c8efaf7e87b4286688bac966fc7e989ce
ayano-w <ayasaku.t5@gmail.com>
2021-10-19 13:19:16 +0000

    [Add]add posting adn deleting, liking functions

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '02de9f86ee09b78bda27be6c700132eeaaf45ad0e671fbdb86b884e5a57fcf9e2a584a15e6c11d2cdc70bbb97e34554496e9abdb0710ef8b2376ffeb5e5e37bf'
